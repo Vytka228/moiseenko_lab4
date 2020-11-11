aad912db7d91404acf5237bd3e75f04dd77ede5b
Vytka228 <55269572+Vytka228@users.noreply.github.com>
2020-11-11 10:33:34 +0300

    lab4

---
+++ WebApp/CourseProject/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    // Контроллер начальной страницы
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [ResponseCache(CacheProfileName = "NoCache")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(CacheProfileName = "NoCache")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApp/CourseProject/Models/Owner.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    // Класс, представляющий запись в таблице Владельцы
+    public partial class Owner
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Fio { get; set; }
+        public string NameFone { get; set; }
+    }
+}
